a15cf5a0ff7451f1a55c878989a0cb0ad8ee7000
Deepak Shkaya <deepakshakya4455@gmail.com>
2020-06-24 02:29:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './Person/Form'
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  useEffect(()=>{
+    document.title='Weather App'
+  })
+  return(
+    <div>
+    <Form />
     </div>
-  );
+  )
 }
 
-export default App;
+  export default App
